c0af708a4ef0a268cdf3072497907e5488981698
NolwaziMM <mkhizenolwazi4@gmail.com>
2022-12-13 21:47:13 -0800

    nested loops

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -14,15 +14,34 @@ void times_table(void)
 
 		for (mult = 1; mult <= 9; mult++)
 		{
-			_putchar(' ');
-			_putchar(',');
-
 			prod = num * mult;
-
-			if (prod <= 9)
-				_putchar(' ');
+			if ((prod / 10) == 0)
+			{
+				if (mult == 0)
+				{
+					_putchar('0');
+				}
+				if (mult != 0)
+				{
+					_putchar(' ');
+					_putchar((prod % 10) + '0');
+				}
+				if (mult < 9)
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
+			}
 			else
+			{
 				_putchar((prod / 10) + '0');
+				_putchar((prod % 10) + '0');
+				if (mult < 9)
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
+			}
 		}
 		_putchar('\n');
 	}
