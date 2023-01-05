8e252c65ef3504ba4fb49e4a715a24839cade221
NolwaziMM <mkhizenolwazi4@gmail.com>
2023-01-05 03:46:28 -0800

    advanced tasks

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -6,23 +6,21 @@
  * @b: The number to test for the square root of @a
  * Return: square root
  */
-
 int helper(int a, int b)
 {
 	if (b * b > a)
 		return (-1);
 	else if (b * b == a)
 		return (b);
-	else 
+	else
 		return (helper(a, b + 1));
 	return (1);
 }
 /**
  * _sqrt_recursion - returns the natural square root of a number
- * @n: the number
+ * @n: the number to return the square root of
  * Return: the square root of @n
  */
-
 int _sqrt_recursion(int n)
 {
 	if (n == 0)
